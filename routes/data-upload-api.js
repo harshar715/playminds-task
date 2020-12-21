51bb032b0bc8e71859ce695fa8f238b69df1e6fd
Harsha R <Harsha.R3@mindtree.com>
2020-12-21 08:56:54 +0530

    first commit

--- routes/data-upload-api.js
+++ routes/data-upload-api.js
@@ -197,87 +197,87 @@ router.post('/loadCSVData', function (req, res, next) {
 
 });
 
-// router.post('/loadCSVDataWithThreads', function (req, res, next) {
+router.post('/loadCSVDataWithThreads', function (req, res, next) {
 
-//     const worker = new Worker(`
-//     const mongoose = require('mongoose');
+    const worker = new Worker(`
+    const mongoose = require('mongoose');
 
-//     const csv = require('csv-parser');
-//     const fs = require('fs');
-//     const Agent = require('./models/agent-model');
-//     const User = require('./models/user-model');
-//     const UserAccount = require('./models/user-account-model');
-//     const PolicyCarrier = require('./models/policy-carrier-model');
-//     const PolicyCategory = require('./models/policy-category-model');
-//     const Policy = require('./models/policy-model');
+    const csv = require('csv-parser');
+    const fs = require('fs');
+    const Agent = require('./models/agent-model');
+    const User = require('./models/user-model');
+    const UserAccount = require('./models/user-account-model');
+    const PolicyCarrier = require('./models/policy-carrier-model');
+    const PolicyCategory = require('./models/policy-category-model');
+    const Policy = require('./models/policy-model');
 
-//     fs.createReadStream('./data.csv')
-//         .pipe(csv())
-//         .on('data', async (row) => {
+    fs.createReadStream('./data.csv')
+        .pipe(csv())
+        .on('data', async (row) => {
 
-//             const agent = {
-//                 _id: new mongoose.Types.ObjectId(),
-//                 angentName: row.agent
-//             };
-//             const user = {
-//                 _id: new mongoose.Types.ObjectId(),
-//                 firstName: row.firstname,
-//                 dateOfBirth: row.dob,
-//                 address: row.address,
-//                 phoneNumber: row.phone,
-//                 city: row.city,
-//                 state: row.state,
-//                 zipCode: row.zip,
-//                 email: row.email,
-//                 gender: row.gender,
-//                 userType: row.userType
-//             };
-//             const useraccount = {
-//                 _id: new mongoose.Types.ObjectId(),
-//                 accountName: row.account_name,
-//                 accountType: row.account_type,
-//                 userId: user._id
-//             };
-//             const policycarrier = {
-//                 _id: new mongoose.Types.ObjectId(),
-//                 policyCarrierName: row.company_name,
-//                 policyCarrierCSR: row.csr,
-//                 policyCarrierProducer: row.producer
-//             };
-//             const policycategory = {
-//                 _id: new mongoose.Types.ObjectId(),
-//                 policyCategoryName: row.category_name
-//             };
-//             const policy = {
-//                 policyNumber: row.policy_number,
-//                 policyType: row.policy_type,
-//                 policyMode: row.policy_mode,
-//                 policyPremiumAmount: row.premium_amount,
-//                 policyStartDate: row.policy_start_date,
-//                 policyEndDate: row.policy_end_date,
-//                 policyCategory: policycategory._id,
-//                 policyCarrierId: policycarrier._id,
-//                 userId: user._id,
-//                 agentId: agent._id
-//             };
-//             Agent.create(agent).then().catch();
-//             User.create(user).then().catch();
-//             UserAccount.create(useraccount).then().catch();
-//             PolicyCarrier.create(policycarrier).then().catch();
-//             PolicyCategory.create(policycategory).then().catch();
-//             Policy.create(policy).then().catch();
+            const agent = {
+                _id: new mongoose.Types.ObjectId(),
+                angentName: row.agent
+            };
+            const user = {
+                _id: new mongoose.Types.ObjectId(),
+                firstName: row.firstname,
+                dateOfBirth: row.dob,
+                address: row.address,
+                phoneNumber: row.phone,
+                city: row.city,
+                state: row.state,
+                zipCode: row.zip,
+                email: row.email,
+                gender: row.gender,
+                userType: row.userType
+            };
+            const useraccount = {
+                _id: new mongoose.Types.ObjectId(),
+                accountName: row.account_name,
+                accountType: row.account_type,
+                userId: user._id
+            };
+            const policycarrier = {
+                _id: new mongoose.Types.ObjectId(),
+                policyCarrierName: row.company_name,
+                policyCarrierCSR: row.csr,
+                policyCarrierProducer: row.producer
+            };
+            const policycategory = {
+                _id: new mongoose.Types.ObjectId(),
+                policyCategoryName: row.category_name
+            };
+            const policy = {
+                policyNumber: row.policy_number,
+                policyType: row.policy_type,
+                policyMode: row.policy_mode,
+                policyPremiumAmount: row.premium_amount,
+                policyStartDate: row.policy_start_date,
+                policyEndDate: row.policy_end_date,
+                policyCategory: policycategory._id,
+                policyCarrierId: policycarrier._id,
+                userId: user._id,
+                agentId: agent._id
+            };
+            Agent.create(agent).then().catch();
+            User.create(user).then().catch();
+            UserAccount.create(useraccount).then().catch();
+            PolicyCarrier.create(policycarrier).then().catch();
+            PolicyCategory.create(policycategory).then().catch();
+            Policy.create(policy).then().catch();
 
 
-//         })
-//         .on('end', () => {
-//             console.log('CSV file successfully processed');
-//         });
-//     `, { eval: true });
-//     worker.on('exit', (code) => {
-//         res.send('CSV file successfully processed');
-//     });
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+        });
+    `, { eval: true });
+    worker.on('exit', (code) => {
+        res.send('CSV file successfully processed');
+    });
 
-// });
+});
 
 module.exports = router;
 
